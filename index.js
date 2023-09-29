1c96bc3f99fd19364d5ca17a90466ef7dbdd2eca
Rachel Hamby <tnthreat33@gmail.com>
2023-09-28 20:27:32 -0400

    commit tree 8f8e7e6e86f9e898a65e6b4d399530e2a884d08a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+const random = Math.floor( Math.random() * 100 ) + 1;
+
+let num3 = 22
+let num4 = 6
+
+let mod = num3%num4
+
+let arr = [1,5,20]
+let max = Math.max(1,2,20)
